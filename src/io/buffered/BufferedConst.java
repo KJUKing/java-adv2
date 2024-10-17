7b93db06e4c07778bf2d39657c557019cb7aacdf
KJUKing <kyungjurhdqn@gmail.com>
2024-10-17 21:46:58 +0900

    241017

---
+++ src/io/buffered/BufferedConst.java
@@ -0,0 +1,7 @@
+package io.buffered;
+
+public class BufferedConst {
+    public static final String FILE_NAME = "temp/buffered.dat";
+    public static final int FILE_SIZE = 10 * 1024 * 1024; // 10mb 1024바이트 x 1024바이트 = 1mb인데 거기서 10을 곱한거다
+    public static final int BUFFER_SIZE = 8192; // 8kb
+}
